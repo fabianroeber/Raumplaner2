d88d42bc503f587310474cb4d39de114ea073ebf
Alex <Alex@Alex-Tablet>
2014-11-14 14:04:27 +0100

    initial GUI commit

--- Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
+++ Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
@@ -1,6 +1,9 @@
 package itprojekt.raumplaner.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -9,8 +12,9 @@ public class Raumplaner implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-genserated method stub
-
+		final Button sendButton = new Button("Send");
+		final TextBox nameField = new TextBox();
+		nameField.setText("Textfeld");
+		final Label errorLabel = new Label();
 	}
-
 }
