32dc082e57ce85aa068f69378677d56b88ec4db4
Alex <Alex@Alex-Tablet>
2014-11-15 16:09:26 +0100

    neue Struktur im GUI

--- Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
+++ Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
@@ -5,6 +5,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -14,14 +15,18 @@ public class Raumplaner implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 
-		final Button sendButton = new Button(
-				"Hallo Alex dein Test funktioniert jetzt");
+		VerticalPanel vPanel = new VerticalPanel();
+		
+		final Button sendButton = new Button("Hallo Alex dein Test funktioniert jetzt");
+		vPanel.add(sendButton);
+		
 		final TextBox nameField = new TextBox();
+		vPanel.add(nameField);
 		nameField.setText("Textfeld");
+		
 		final Label errorLabel = new Label();
+		vPanel.add(errorLabel);
 
-		RootPanel.get().add(errorLabel);
-		RootPanel.get().add(sendButton);
-		RootPanel.get().add(nameField);
+		RootPanel.get().add(vPanel);
 	}
 }
