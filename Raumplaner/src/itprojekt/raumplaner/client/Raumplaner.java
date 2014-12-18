425bc7674e8bd0dfcfa66a55c42ead4da5c98a90
Fabian Röber <fabianroeber@aol.com>
2014-12-18 01:03:30 +0100

    UiBinder für Grundlayout

--- Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
+++ Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
@@ -15,18 +15,8 @@ public class Raumplaner implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 
-		VerticalPanel vPanel = new VerticalPanel();
-		
-		final Button sendButton = new Button("Hallo Alex dein Test funktioniert jetzt");
-		vPanel.add(sendButton);
-		
-		final TextBox nameField = new TextBox();
-		vPanel.add(nameField);
-		nameField.setText("Textfeld");
-		
-		final Label errorLabel = new Label();
-		vPanel.add(errorLabel);
+		RootLayout layout = new RootLayout();
 
-		RootPanel.get().add(vPanel);
+		RootPanel.get().add(layout);
 	}
 }
