42105dcfc6313e2ba1a3ee43915794be9929d7c8
Feridun Mekec <fm039@hdm-stuttgart.de>
2014-12-21 17:40:37 +0100

    Neue GUI-Klasse hinzugefügt. Diese Klasse implementiert den EntryPoint und ist als "HauptKlasse/DemoKlasse" gedacht. In der Methode onModuleLoad wird der Konstruktor von der Klasse HauptLayout aufgerufen, welcher dann dem RootPanel als Variable hl dann übergeben wird.

--- Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
+++ Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
@@ -1,11 +1,7 @@
 package itprojekt.raumplaner.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -15,8 +11,9 @@ public class Raumplaner implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 
-		RootLayout layout = new RootLayout();
-
-		RootPanel.get().add(layout);
+		HauptLayout hl = new HauptLayout();
+		
+		
+		RootPanel.get().add(hl);
 	}
 }
