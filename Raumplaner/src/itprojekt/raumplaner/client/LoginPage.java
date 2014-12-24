83108d7a66e6a9cbd1f7d001cc191780b3f1a665
Feridun Mekec <fm039@hdm-stuttgart.de>
2014-12-24 15:54:41 +0100

    GUI - Bug mit den PW-Boxes und textBoxes beseitigt. Methoden zum Stylen der einzelnen Widgets in der FlexTable hinzugefügt. AnmeldeButton in einem PowerIcon-Form hinzugefügt und ihm einen ClickHandler zugewiesen, welcher einen WindowAlert firet, wenn er geklickt wird. HdM Logo am Ende der Flextable hinzugefügt

--- Raumplaner/src/itprojekt/raumplaner/client/LoginPage.java
+++ Raumplaner/src/itprojekt/raumplaner/client/LoginPage.java
@@ -1,9 +1,14 @@
 package itprojekt.raumplaner.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
 import com.google.gwt.user.client.ui.PasswordTextBox;
@@ -16,6 +21,11 @@ public class LoginPage extends Composite {
 	private Label anmelden;
 	private Label userName;
 	private Label password;
+	
+//	private Button anmeldeButton;
+	
+	Image powerIcon;
+	Image hdmLogo;
 	
 	TextBox eingabeBenutzerName;
 	PasswordTextBox eingabeBenutzerPasswort;
@@ -36,6 +46,7 @@ public class LoginPage extends Composite {
 //		Titel der FlexTable hinzufügen
 		
 		anmelden = new Label("Bitte melden Sie sich an");
+		anmelden.getElement().setAttribute("id", "headerAnmelden-id");
 		
 		flex.setWidget(0, 0, anmelden);
 		formatter.setColSpan(0, 0, 4);
@@ -45,6 +56,7 @@ public class LoginPage extends Composite {
 		
 		userName = new Label("Username:");
 		eingabeBenutzerName = new TextBox();
+		eingabeBenutzerName.setPixelSize(125, 15);
 		
 		flex.setWidget(1, 0, userName);
 		flex.setWidget(1, 1, eingabeBenutzerName);
@@ -52,15 +64,53 @@ public class LoginPage extends Composite {
 		
 		password = new Label("Password:");
 		eingabeBenutzerPasswort = new PasswordTextBox();
+		eingabeBenutzerPasswort.setPixelSize(125, 15);
 		
 		flex.setWidget(2, 0, password);
 		flex.setWidget(2, 1, eingabeBenutzerPasswort);
 		
+//		Einen Button hinzufügen
+		
+//		anmeldeButton = new Button("Anmelden");
+		
+//		flex.setWidget(2, 4, anmeldeButton);
+		
+//		Power-Button Icon neben dem anmeldeButton hinzufügen
+		
+		powerIcon = new Image("images/Power-Button.jpg");
+		powerIcon.setPixelSize(30, 30);
+		
+		powerIcon.addClickHandler(new PowerButtonHandler());
+		
+		flex.setWidget(2, 3, powerIcon);
+		
+		Label anmeldeText = new Label("Anmelden");
+		flex.setWidget(2, 4, anmeldeText);
+		
+		
+//		Versuch HdM-Logo der LoginSeite zu hinzufügen - VORLÄUFIG
+		
+		hdmLogo = new Image("images/HdM Logo.png");
+		hdmLogo.setPixelSize(150, 70);
+		
+		flex.setWidget(4, 4, hdmLogo);
 		
 		
 		loginPanel.setWidget(flex);
 		
 		
+	}
+	
+//	Versuch, dem PowerIcon einen Handler zuzuweisen
+	
+	private class PowerButtonHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+			Window.alert("Sie sind nun angemeldet");
+			
+		}
 		
 	}
 
