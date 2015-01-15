414bfef036cba5dff39458be4c7acd6ff7dc5baa
Feridun Mekec <fm039@hdm-stuttgart.de>
2015-01-15 22:17:16 +0100

    GUI - ClickHandler für die neue Klasse NeueBuchung hinzugefügt.

--- Raumplaner/src/itprojekt/raumplaner/client/RaumBuchungsMenu.java
+++ Raumplaner/src/itprojekt/raumplaner/client/RaumBuchungsMenu.java
@@ -1,5 +1,7 @@
 package itprojekt.raumplaner.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -11,15 +13,30 @@ public class RaumBuchungsMenu extends Composite {
 	Anchor neueBuchung;
 	Anchor buchungBearbeiten;
 	
+	NeueBuchung neuBuchen;
+	
 	public RaumBuchungsMenu() {
 		
 		initWidget(this.raumMenuPanel);
 		
 		neueBuchung = new Anchor("Neue Buchung");
+		neueBuchung.addClickHandler(new neueBuchungHandler());
+		
 		buchungBearbeiten = new Anchor("Buchung bearbeiten");
 		
 		raumMenuPanel.add(neueBuchung);
 		raumMenuPanel.add(buchungBearbeiten);
+		
+	} private class neueBuchungHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			neuBuchen = new NeueBuchung();
+			
+			raumMenuPanel.add(neuBuchen);
+			
+		}
+		
 		
 	}
 
