84ff29682778fa04998c2a0bfc6a22835ea028fe
Feridun Mekec <fm039@hdm-stuttgart.de>
2015-01-19 15:21:44 +0100

    GUI - Header hinzugefügt

--- Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
+++ Raumplaner/src/itprojekt/raumplaner/client/Raumplaner.java
@@ -1,6 +1,7 @@
 package itprojekt.raumplaner.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -11,12 +12,18 @@ public class Raumplaner implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		
+		Label raumplanerHeader = new Label("Raumreservierungssystem");
+		
+		raumplanerHeader.getElement().setAttribute("id", "RRS-id");
+		
+		RootPanel.get().add(raumplanerHeader);
+		
 //		neues GUI:
 		
 		Navigation navigation = new Navigation();
 		
 		RootPanel.get().add(navigation);
-
+		
 //		Für das alte GUI hier ausklammern:
 		
 //		LoginPage anmelden = new LoginPage();
