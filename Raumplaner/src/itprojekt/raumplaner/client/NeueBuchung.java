05c0f11f6baab82d309dcd5d1a3faa6e86bc0314
Feridun Mekec <fm039@hdm-stuttgart.de>
2015-01-20 14:07:26 +0100

    GUI - Handler hinzugefügt und Bug mit dem mehrmaligen Klicken gefixt.

--- Raumplaner/src/itprojekt/raumplaner/client/NeueBuchung.java
+++ Raumplaner/src/itprojekt/raumplaner/client/NeueBuchung.java
@@ -1,9 +1,13 @@
 package itprojekt.raumplaner.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -11,6 +15,8 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class NeueBuchung extends Composite {
 	
 	VerticalPanel neueBuchungPnl = new VerticalPanel();
+	HorizontalPanel pnlMitUser = new HorizontalPanel();
+	VerticalPanel eingeladenePnl = new VerticalPanel();
 	
 	Label raumAuswaehlen;
 	ListBox raeumeDropDown; 
@@ -18,13 +24,15 @@ public class NeueBuchung extends Composite {
 	Label zeitSlot;
 	ListBox ZeitSlotDropDown;
 	
-	Label personenAuswählen;
+	Label personenAuswaehlen;
 	SuggestBox personenAuswahl;
+	Button personAdden;
 	
 	Label einladungsThema;
 	TextBox themaEintragen;
 	
 	Button raumBuchen;
+	BuchungErfolgreich erfolgreichGebucht;
 	
 	public NeueBuchung() {
 		
@@ -52,31 +60,59 @@ public class NeueBuchung extends Composite {
 		ZeitSlotDropDown.addItem("14:15 - 17:15 Uhr");
 		ZeitSlotDropDown.addItem("17:15 - 20:15 Uhr");
 		
-		personenAuswählen = new Label("Personen auswählen:");
+		personenAuswaehlen = new Label("Personen auswählen:");
 		
 		personenAuswahl = new SuggestBox(Personen.personenEinladen());
 		
+		personAdden = new Button("OK");
+		personAdden.setPixelSize(30, 30);
+		personAdden.addClickHandler(new PersonAddenHandler());
+		
+		pnlMitUser.add(personenAuswahl);
+		pnlMitUser.add(personAdden);
 		
 		einladungsThema = new Label("Tragen Sie das Thema ein:");
 		
 		themaEintragen = new TextBox();
 		
 		raumBuchen = new Button("Raum buchen");
+		raumBuchen.addClickHandler(new erfolgreicheBuchungHandler());
 		
 		neueBuchungPnl.add(raumAuswaehlen);
 		neueBuchungPnl.add(raeumeDropDown);
 		neueBuchungPnl.add(zeitSlot);
 		neueBuchungPnl.add(ZeitSlotDropDown);
-		neueBuchungPnl.add(personenAuswählen);
-		neueBuchungPnl.add(personenAuswahl);
+		neueBuchungPnl.add(personenAuswaehlen);
+		neueBuchungPnl.add(eingeladenePnl);
+		neueBuchungPnl.add(pnlMitUser);
 		neueBuchungPnl.add(einladungsThema);
 		neueBuchungPnl.add(themaEintragen);
 		neueBuchungPnl.add(raumBuchen);
 		
-		
-		
+	} public class PersonAddenHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+			Label personen = new Label("- " + personenAuswahl.getText() + " ist eingeladen");
+			
+			eingeladenePnl.add(personen);
+			
+		}
+			
+	   } public class erfolgreicheBuchungHandler implements ClickHandler {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				erfolgreichGebucht = new BuchungErfolgreich();
+				
+				RootPanel.get().add(erfolgreichGebucht);
+				
+			}
+			
+			
+			
+		}
 		
 		
 	}
-
-}
