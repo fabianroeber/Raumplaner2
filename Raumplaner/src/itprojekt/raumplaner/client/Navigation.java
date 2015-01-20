20dc1fb77d26880755f6619d31188710d0972e2a
Feridun Mekec <fm039@hdm-stuttgart.de>
2015-01-20 14:06:26 +0100

    GUI - ClickHandler hinzugefügt

--- Raumplaner/src/itprojekt/raumplaner/client/Navigation.java
+++ Raumplaner/src/itprojekt/raumplaner/client/Navigation.java
@@ -1,11 +1,14 @@
 package itprojekt.raumplaner.client;
 
+import org.apache.tools.ant.taskdefs.Get;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Navigation extends Composite {
@@ -16,6 +19,8 @@ public class Navigation extends Composite {
 	Label navHeader;
 	Anchor raumBuchung;
 	Anchor raumVerfuegbarkeit;
+	
+	Raumverfuegabarkeit verfuegbareRaeume;
 	
 	RaumBuchungsMenu raumBuchenMenu;
 	
@@ -31,6 +36,7 @@ public class Navigation extends Composite {
 		raumBuchung.addClickHandler(new raumBuchungHandler());
 		
 		raumVerfuegbarkeit = new Anchor("Raumverfügbarkeit überprüfen");
+		raumVerfuegbarkeit.addClickHandler(new verfuegbarkeitHandler());
 		
 		zusammenPanel.setSpacing(20);
 
@@ -49,9 +55,26 @@ public class Navigation extends Composite {
 			
 			
 			zusammenPanel.add(raumBuchenMenu);
+			raumBuchung.setVisible(false);
 			
 			
 		}
+		
+		
+	} private class verfuegbarkeitHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+			verfuegbareRaeume = new Raumverfuegabarkeit();
+			
+			zusammenPanel.add(verfuegbareRaeume);
+			
+			raumBuchung.setVisible(true);
+			raumVerfuegbarkeit.setVisible(false);
+			
+		}
+		
 		
 		
 	}
